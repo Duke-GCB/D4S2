433084b5b1166ad6e3a96c72155eb705c4416e75
Dan Leehr <dan.leehr@duke.edu>
2016-03-29 11:26:29 -0400

    Tests for drafts

--- handoverservice/handover_api/tests_views.py
+++ handoverservice/handover_api/tests_views.py
@@ -48,4 +48,47 @@ class HandoverViewTestCase(APITestCase):
         self.assertEqual(h.project_id, 'project3')
 
 
+class DraftViewTestCase(APITestCase):
+
+    def test_create_draft(self):
+        url = reverse('draft-list')
+        data = {'project_id':'project-id-2', 'from_user_id': 'user1', 'to_user_id': 'user2'}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Draft.objects.count(), 1)
+        self.assertEqual(Draft.objects.get().from_user_id, 'user1')
+
+    def test_list_drafts(self):
+        Draft.objects.create(project_id='project1', from_user_id='fromuser1', to_user_id='touser1')
+        Draft.objects.create(project_id='project2', from_user_id='fromuser1', to_user_id='touser1')
+        url = reverse('draft-list')
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 2)
+
+    def test_get_draft(self):
+        d =  Draft.objects.create(project_id='project1', from_user_id='fromuser1', to_user_id='touser1')
+        url = reverse('draft-detail', args=(d.pk,))
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['project_id'], 'project1')
+
+    def test_delete_draft(self):
+        d =  Draft.objects.create(project_id='project2', from_user_id='fromuser1', to_user_id='touser1')
+        url = reverse('draft-detail', args=(d.pk,))
+        response = self.client.delete(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Draft.objects.count(), 0)
+
+    def test_update_draft(self):
+        d =  Draft.objects.create(project_id='project2', from_user_id='fromuser1', to_user_id='touser1')
+        updated = {'project_id': 'project3', 'from_user_id': 'fromuser1', 'to_user_id':'touser1'}
+        url = reverse('draft-detail', args=(d.pk,))
+        response = self.client.put(url, data=updated, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        d =  Draft.objects.get(pk=d.pk)
+        self.assertEqual(d.project_id, 'project3')
+
+
+
 
