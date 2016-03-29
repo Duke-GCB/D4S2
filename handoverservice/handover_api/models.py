4db05425bad6383e7310505413697f65ae7ff395
Dan Leehr <dan.leehr@duke.edu>
2016-03-29 10:58:09 -0400

    Rework choices for states, and write tests for initial models

--- handoverservice/handover_api/models.py
+++ handoverservice/handover_api/models.py
@@ -3,24 +3,41 @@ from __future__ import unicode_literals
 from django.db import models
 
 class User(models.Model):
-    dds_id = models.CharField(max_length=36)
-    api_key = models.CharField(max_length=36)
+    dds_id = models.CharField(max_length=36, null=False, unique=True)
+    api_key = models.CharField(max_length=36, null=False)
+
+
+class State(object):
+    INITIATED = 0
+    NOTIFIED = 1
+    ACCEPTED = 2
+    REJECTED = 3
+    HANDOVER_CHOICES = (
+        (INITIATED, 'Initiated'),
+        (NOTIFIED, 'Notified'),
+        (ACCEPTED, 'Accepted'),
+        (REJECTED, 'Rejected'),
+    )
+    DRAFT_CHOICES = (
+        (NOTIFIED, 'Notified'),
+    )
 
 
 class Handover(models.Model):
-    STATE_CHOICES = ((0, 'Initiated'),
-                     (1,'Notified',),
-                     (2,'Accepted'),
-                     (3,'Rejected'),)
-    project_id = models.CharField(max_length=36)
-    from_user_id = models.CharField(max_length=36)
-    to_user_id = models.CharField(max_length=36)
-    state = models.IntegerField(choices=STATE_CHOICES, default=STATE_CHOICES[0])
+    project_id = models.CharField(max_length=36, null=False)
+    from_user_id = models.CharField(max_length=36, null=False)
+    to_user_id = models.CharField(max_length=36, null=False)
+    state = models.IntegerField(choices=State.HANDOVER_CHOICES, default=State.INITIATED, null=False)
 
+    class Meta:
+        unique_together = ('project_id', 'from_user_id', 'to_user_id')
 
 class Draft(models.Model):
     STATE_CHOICES = ((1,'Notified',),)
-    project_id = models.CharField(max_length=36)
-    from_user_id = models.CharField(max_length=36)
-    to_user_id = models.CharField(max_length=36)
-    state = models.IntegerField(choices=STATE_CHOICES, default=STATE_CHOICES[0])
+    project_id = models.CharField(max_length=36, null=False)
+    from_user_id = models.CharField(max_length=36, null=False)
+    to_user_id = models.CharField(max_length=36, null=False)
+    state = models.IntegerField(choices=State.DRAFT_CHOICES, default=State.NOTIFIED, null=False)
+
+    class Meta:
+        unique_together = ('project_id', 'from_user_id', 'to_user_id')
