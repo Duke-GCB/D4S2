8a3fc68f5ea217733917f49c7902bd9f2486de86
Dan Leehr <dan.leehr@duke.edu>
2016-03-30 15:56:05 -0400

    Tests for fetching project details

--- handoverservice/mail_draft/tests_ddsutil.py
+++ handoverservice/mail_draft/tests_ddsutil.py
@@ -7,9 +7,10 @@ from mail_draft.dds_util import DDSUtil
 
 
 class DDSUtilTestCase(TestCase):
+    def setUp(self):
+        self.user_id = 'abcd-1234-efgh-8876'
     @mock.patch('ddsc.core.remotestore.RemoteStore')
     def testGetEmail(self, mockRemoteStore):
-        user_id = 'abcd-1234-efgh-8876'
         email = 'example@domain.com'
         # Mock a remote user object, and bind it to fetch_user
         remote_user = mock.Mock()
@@ -23,12 +24,35 @@ class DDSUtilTestCase(TestCase):
             # Python 3
             import importlib
             importlib.reload(mail_draft.dds_util)
-        User.objects.create(dds_id=user_id, api_key='uhn3wk7h24ighg8i2')
+        User.objects.create(dds_id=self.user_id, api_key='uhn3wk7h24ighg8i2')
         # DDSUtil reads settings from django settings, so inject some here
         with self.settings(DDSCLIENT_PROPERTIES={}):
-            ddsutil = DDSUtil(user_id)
-            self.assertEqual(email, ddsutil.get_remote_user(user_id).email)
+            ddsutil = DDSUtil(self.user_id)
+            self.assertEqual(email, ddsutil.get_remote_user(self.user_id).email)
             self.assertTrue(instance.fetch_user.called)
+
+    @mock.patch('ddsc.core.remotestore.RemoteStore')
+    def testGetProject(self, mockRemoteStore):
+        project_id = '8677-11231-44414-4442'
+        project_name = 'Project ABC'
+        remote_project = mock.Mock()
+        remote_project.name = project_name
+        instance = mockRemoteStore.return_value
+        instance.fetch_remote_project_by_id.return_value = remote_project
+
+        # Only import DDSUtil once we've patched RemoteStore
+        try:
+            reload(mail_draft.dds_util)
+        except NameError:
+            # Python 3
+            import importlib
+            importlib.reload(mail_draft.dds_util)
+        User.objects.create(dds_id=self.user_id, api_key='uhn3wk7h24ighg8i2')
+        # DDSUtil reads settings from django settings, so inject some here
+        with self.settings(DDSCLIENT_PROPERTIES={}):
+            ddsutil = DDSUtil(self.user_id)
+            self.assertEqual(ddsutil.get_remote_project(project_id).name, project_name)
+            self.assertTrue(instance.fetch_remote_project_by_id.called)
 
     def testFailsWithoutAPIKeyUser(self):
         with self.settings(DDSCLIENT_PROPERTIES={}):
