72c35d980ce6f23e3532dd14a844a6cbbb3e00e0
Dan Leehr <dan.leehr@duke.edu>
2016-04-05 11:29:47 -0400

    Fixes mock patches in tests_ddsutil
    Remember - patch into the namespace where the mock thing will live

--- mail_draft/tests_ddsutil.py
+++ mail_draft/tests_ddsutil.py
@@ -2,14 +2,14 @@ from django.test import TestCase
 from handover_api.models import User
 from django.core.exceptions import ObjectDoesNotExist
 import mock
-import mail_draft
 from mail_draft.dds_util import DDSUtil
 
 
 class DDSUtilTestCase(TestCase):
     def setUp(self):
         self.user_id = 'abcd-1234-efgh-8876'
-    @mock.patch('ddsc.core.remotestore.RemoteStore')
+
+    @mock.patch('mail_draft.dds_util.RemoteStore')
     def testGetEmail(self, mockRemoteStore):
         email = 'example@domain.com'
         # Mock a remote user object, and bind it to fetch_user
@@ -17,13 +17,6 @@ class DDSUtilTestCase(TestCase):
         remote_user.email = email
         instance = mockRemoteStore.return_value
         instance.fetch_user.return_value = remote_user
-        # Only import DDSUtil once we've patched RemoteStore
-        try:
-            reload(mail_draft.dds_util)
-        except NameError:
-            # Python 3
-            import importlib
-            importlib.reload(mail_draft.dds_util)
         User.objects.create(dds_id=self.user_id, api_key='uhn3wk7h24ighg8i2')
         # DDSUtil reads settings from django settings, so inject some here
         with self.settings(DDSCLIENT_PROPERTIES={}):
@@ -31,7 +24,7 @@ class DDSUtilTestCase(TestCase):
             self.assertEqual(email, ddsutil.get_remote_user(self.user_id).email)
             self.assertTrue(instance.fetch_user.called)
 
-    @mock.patch('ddsc.core.remotestore.RemoteStore')
+    @mock.patch('mail_draft.dds_util.RemoteStore')
     def testGetProject(self, mockRemoteStore):
         project_id = '8677-11231-44414-4442'
         project_name = 'Project ABC'
@@ -39,14 +32,6 @@ class DDSUtilTestCase(TestCase):
         remote_project.name = project_name
         instance = mockRemoteStore.return_value
         instance.fetch_remote_project_by_id.return_value = remote_project
-
-        # Only import DDSUtil once we've patched RemoteStore
-        try:
-            reload(mail_draft.dds_util)
-        except NameError:
-            # Python 3
-            import importlib
-            importlib.reload(mail_draft.dds_util)
         User.objects.create(dds_id=self.user_id, api_key='uhn3wk7h24ighg8i2')
         # DDSUtil reads settings from django settings, so inject some here
         with self.settings(DDSCLIENT_PROPERTIES={}):
