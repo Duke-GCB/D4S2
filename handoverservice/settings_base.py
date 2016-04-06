0be898419f36a474cf33733903734c42274ccede
Dan Leehr <dan.leehr@duke.edu>
2016-04-06 13:31:07 -0400

    Use TokenAuthentication instead of BasicAuthentication

--- handoverservice/settings_base.py
+++ handoverservice/settings_base.py
@@ -119,7 +119,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ('rest_framework.filters.DjangoFilterBackend',),
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ),
 }
