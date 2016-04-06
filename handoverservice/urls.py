a7c9e33cd2824903a104584fb1dd8ffc862103d2
Dan Leehr <dan.leehr@duke.edu>
2016-04-06 13:33:45 -0400

    Add /api-auth to simplify login for browsable interface

--- handoverservice/urls.py
+++ handoverservice/urls.py
@@ -5,5 +5,7 @@ from django.views.generic.base import RedirectView
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/v1/', include('handover_api.urls')),
+    url(r'^api-auth/', include('rest_framework.urls',
+                               namespace='rest_framework')),
     url(r'^$', RedirectView.as_view(url='/api/v1/'))
 ]
