e2cb5e4d02be753d23c92e75f34fbbaab76fb577
Dan Leehr <dan.leehr@duke.edu>
2016-04-07 13:50:37 -0400

    Restores static root to /static/
    Having it at / breaks the development web server

--- handoverservice/settings_base.py
+++ handoverservice/settings_base.py
@@ -105,7 +105,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/'
+STATIC_URL = '/static/'
 
 # Email
 # For development we'll just use the console backend.
