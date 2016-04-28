8e71db5785d76d9a1e77a504cb4f8e38b70f483d
Dan Leehr <dan.leehr@duke.edu>
2016-04-28 16:15:22 -0400

    Add templates and views for local login/logout

--- handoverservice/urls.py
+++ handoverservice/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic.base import RedirectView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -8,5 +9,7 @@ urlpatterns = [
     url(r'^api/v1/', include('handover_api.urls')),
     url(r'^api-auth/', include('rest_framework.urls',
                                namespace='rest_framework')),
+    url(r'^accounts/login/$', auth_views.login, {'template_name': 'ownership/login.html' }),
+    url(r'^accounts/logout/$', auth_views.login, {'template_name': 'ownership/logged_out.html' }),
     url(r'^$', RedirectView.as_view(url='/api/v1/'))
 ]
