f0a309b01f5c655774bd8da91090a11d7cf83095
Dan Leehr <dan.leehr@duke.edu>
2016-06-15 11:28:49 -0400

    Change users route name to dukedsuser

--- handover_api/urls.py
+++ handover_api/urls.py
@@ -5,7 +5,7 @@ from handover_api import views
 router = routers.DefaultRouter()
 router.register(r'handovers', views.HandoverViewSet, 'handover')
 router.register(r'drafts', views.DraftViewSet, 'draft')
-router.register(r'users', views.UserViewSet, 'user')
+router.register(r'users', views.UserViewSet, 'dukedsuser')
 
 urlpatterns = [
     url(r'^', include(router.urls)),
