68484294db6cafd3b91b876ab92bc2830c24812a
Dan Leehr <dan.leehr@duke.edu>
2016-06-15 11:15:43 -0400

    Remove StringRelatedField in favor of SlugRelatedFIeld
    SlugRelatedField is more appropriate

--- handover_api/serializers.py
+++ handover_api/serializers.py
@@ -1,12 +1,12 @@
-from handover_api.models import Handover, Draft, DukeDSUser
+from handover_api.models import Handover, Draft, DukeDSUser, DukeDSProject
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
 
 class HandoverSerializer(serializers.HyperlinkedModelSerializer):
-    project_id = serializers.StringRelatedField(source='project.project_id', read_only=True)
-    from_user_id = serializers.StringRelatedField(source='from_user.dds_id', read_only=True)
-    to_user_id = serializers.StringRelatedField(source='to_user.dds_id', read_only=True)
+    project_id = serializers.SlugRelatedField(source='project', slug_field='project_id', queryset=DukeDSProject.objects.all())
+    from_user_id = serializers.SlugRelatedField(source='from_user', slug_field='dds_id', queryset=DukeDSUser.api_users.all())
+    to_user_id = serializers.SlugRelatedField(source='to_user', slug_field='dds_id', queryset=DukeDSUser.objects.all())
 
     class Meta:
         model = Handover
