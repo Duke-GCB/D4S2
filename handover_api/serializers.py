9d3ec48594c11a58e7053bb1e9ac4cb321fc5bdf
Dan Leehr <dan.leehr@duke.edu>
2016-06-15 10:18:27 -0400

    Add read-only related field serializers for project/from/to
    Just a stop-gap, need to implement writing

--- handover_api/serializers.py
+++ handover_api/serializers.py
@@ -4,12 +4,20 @@ from rest_framework import serializers
 
 
 class HandoverSerializer(serializers.HyperlinkedModelSerializer):
+    project_id = serializers.StringRelatedField(source='project.project_id', read_only=True)
+    from_user_id = serializers.StringRelatedField(source='from_user.dds_id', read_only=True)
+    to_user_id = serializers.StringRelatedField(source='to_user.dds_id', read_only=True)
+
     class Meta:
         model = Handover
         fields = ('id','url','project_id','from_user_id','to_user_id','state')
 
 
 class DraftSerializer(serializers.HyperlinkedModelSerializer):
+    project_id = serializers.StringRelatedField(source='project.project_id', read_only=True)
+    from_user_id = serializers.StringRelatedField(source='from_user.dds_id', read_only=True)
+    to_user_id = serializers.StringRelatedField(source='to_user.dds_id', read_only=True)
+
     class Meta:
         model = Draft
         fields = ('id','url','project_id','from_user_id','to_user_id','state')
