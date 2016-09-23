b887b145c698f22c96db179a531c3260730a1843
Dan Leehr <dan.leehr@duke.edu>
2016-09-23 12:05:52 -0400

    Removes legacy /drafts API endpoint

--- d4s2_api/urls.py
+++ d4s2_api/urls.py
@@ -5,8 +5,6 @@ from d4s2_api import views
 router = routers.DefaultRouter()
 router.register(r'deliveries', views.DeliveryViewSet, 'delivery')
 router.register(r'shares', views.ShareViewSet, 'share')
-# Previous name was drafts, so we include this for compatibility
-router.register(r'drafts', views.ShareViewSet, 'draft')
 router.register(r'users', views.UserViewSet, 'dukedsuser')
 
 urlpatterns = [
