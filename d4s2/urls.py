113d7470fcb840833f81a40d0f33d3aebfbf9170
Dan Leehr <dan.leehr@duke.edu>
2016-11-29 10:42:33 -0500

    Moves login/logged_out templates to auth app

--- d4s2/urls.py
+++ d4s2/urls.py
@@ -14,8 +14,8 @@ urlpatterns = [
                                namespace='rest_framework')),
     url(r'^api-token-auth/', authtoken_views.obtain_auth_token),
     # ownership-themed login/logout pages
-    url(r'^accounts/login/$', auth_views.login, {'template_name': 'ownership/login.html' }, name='login'),
-    url(r'^accounts/logout/$', auth_views.logout, {'template_name': 'ownership/logged_out.html' }, name='logout'),
+    url(r'^accounts/login/$', auth_views.login, {'template_name': 'd4s2_auth/login.html' }, name='login'),
+    url(r'^accounts/logout/$', auth_views.logout, {'template_name': 'd4s2_auth/logged_out.html' }, name='logout'),
     # Redirect / to /accounts/login
     url(r'^$', RedirectView.as_view(pattern_name='auth-home', permanent=False)),
 ]
