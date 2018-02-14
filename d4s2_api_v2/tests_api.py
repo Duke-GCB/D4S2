c78e9eaf2bd9c12f65b07a3bf0b5c4e1c20b22be
John Bradley <johnbradley2008@gmail.com>
2018-02-14 17:02:06 -0500

    add tests for duke-ds-project-transfers api

--- d4s2_api_v2/tests_api.py
+++ d4s2_api_v2/tests_api.py
@@ -244,3 +244,134 @@ class DDSProjectsViewSetTestCase(AuthenticatedResourceTestCase):
         self.assertEqual(project['id'], 'project1')
         self.assertEqual(project['name'], 'Mouse')
         self.assertEqual(project['description'], 'Mouse RNA')
+
+
+class DDSProjectTransfersViewSetTestCase(AuthenticatedResourceTestCase):
+    def test_fails_unauthenticated(self):
+        self.client.logout()
+        url = reverse('v2-dukedsprojecttransfer-list')
+        response = self.client.post(url, {}, format='json')
+        self.assertUnauthorized(response)
+
+    def test_post_not_permitted(self):
+        url = reverse('v2-dukedsprojecttransfer-list')
+        data = {}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_put_not_permitted(self):
+        url = reverse('v2-dukedsprojecttransfer-list')
+        data = {}
+        response = self.client.put(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    @patch('d4s2_api_v2.api.DDSUtil')
+    def test_list_transfers(self, mock_dds_util):
+        mock_response = Mock()
+        mock_response.json.return_value = {
+            'results': [
+                {
+                    'id': 'transfer1',
+                    'status': 'pending',
+                    'status_comment': 'Some status comment',
+                    'to_users': [
+                        {
+                            'id': 'user1',
+                            'username': 'joe',
+                            'full_name': 'Joe Bob'
+                        }
+                    ],
+                    'from_user': {
+                        'id': 'user2',
+                        'username': 'jim',
+                        'full_name': 'Jim Bob'
+                    },
+                    'project': {
+                        'id': 'project1',
+                        'name': 'Mouse'
+                    }
+                }, {
+                    'id': 'transfer2',
+                    'status': 'accepted',
+                    'status_comment': None,
+                    'to_users': [
+                        {
+                            'id': 'user1',
+                            'username': 'joe',
+                            'full_name': 'Joe Bob'
+                        }
+                    ],
+                    'from_user': {
+                        'id': 'user3',
+                        'username': 'jane',
+                        'full_name': 'Jane Bob'
+                    },
+                    'project': {
+                        'id': 'project2',
+                        'name': 'Rat'
+                    }
+                }
+            ]
+        }
+        mock_dds_util.return_value.get_project_transfers.return_value = mock_response
+        url = reverse('v2-dukedsprojecttransfer-list')
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 2)
+
+        transfer = response.data[0]
+        self.assertEqual(transfer['id'], 'transfer1')
+        self.assertEqual(transfer['status'], 'pending')
+        self.assertEqual(transfer['status_comment'], 'Some status comment')
+        self.assertEqual(len(transfer['to_users']), 1)
+        self.assertEqual(transfer['to_users'][0]['id'], 'user1')
+        self.assertEqual(transfer['from_user']['id'], 'user2')
+        self.assertEqual(transfer['project']['name'], 'Mouse')
+
+        transfer = response.data[1]
+        self.assertEqual(transfer['id'], 'transfer2')
+        self.assertEqual(transfer['status'], 'accepted')
+        self.assertEqual(transfer['status_comment'], None)
+        self.assertEqual(len(transfer['to_users']), 1)
+        self.assertEqual(transfer['to_users'][0]['id'], 'user1')
+        self.assertEqual(transfer['from_user']['id'], 'user3')
+        self.assertEqual(transfer['project']['name'], 'Rat')
+
+    @patch('d4s2_api_v2.api.DDSUtil')
+    def test_get_project(self, mock_dds_util):
+        mock_response = Mock()
+        mock_response.json.return_value = {
+            'id': 'transfer1',
+            'status': 'pending',
+            'status_comment': 'Some status comment',
+            'to_users': [
+                {
+                    'id': 'user1',
+                    'username': 'joe',
+                    'full_name': 'Joe Bob'
+                }
+            ],
+            'from_user': {
+                'id': 'user2',
+                'username': 'jim',
+                'full_name': 'Jim Bob'
+            },
+            'project': {
+                'id': 'project1',
+                'name': 'Mouse'
+            }
+        }
+        mock_dds_util.return_value.get_project_transfer.return_value = mock_response
+        url = reverse('v2-dukedsprojecttransfer-list') + 'transfer1/'
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        mock_dds_util.return_value.get_project_transfer.assert_called_with('transfer1')
+
+        transfer = response.data
+        self.assertEqual(transfer['id'], 'transfer1')
+        self.assertEqual(transfer['status'], 'pending')
+        self.assertEqual(transfer['status_comment'], 'Some status comment')
+        self.assertEqual(len(transfer['to_users']), 1)
+        self.assertEqual(transfer['to_users'][0]['id'], 'user1')
+        self.assertEqual(transfer['from_user']['id'], 'user2')
+        self.assertEqual(transfer['project']['name'], 'Mouse')
