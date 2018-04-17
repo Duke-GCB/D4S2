0810a4c94bf0e7c1d4ba8735137ceb6e224b8beb
John Bradley <johnbradley2008@gmail.com>
2018-04-17 10:37:38 -0400

    tests for S3DeliveryUtil

--- switchboard/tests_s3_util.py
+++ switchboard/tests_s3_util.py
@@ -1,7 +1,107 @@
 from django.test import TestCase
 from mock import patch, Mock, call
-from d4s2_api.models import S3UserTypes, S3User, S3Delivery, User
-from switchboard.s3_util import S3Resource
+from d4s2_api.models import S3Bucket, S3User, S3UserTypes, S3Delivery, User, S3Endpoint
+from switchboard.s3_util import S3Resource, S3DeliveryUtil
+
+
+class S3DeliveryUtilTestCase(TestCase):
+    def setUp(self):
+        self.user_agent = User.objects.create(username='agent')
+        self.from_user = User.objects.create(username='from_user')
+        self.to_user = User.objects.create(username='to_user')
+        self.endpoint = S3Endpoint.objects.create(url='https://s3service.com/')
+        self.s3_agent_user = S3User.objects.create(endpoint=self.endpoint,
+                                                   s3_id='agent_s3_id',
+                                                   user=self.user_agent,
+                                                   type=S3UserTypes.AGENT)
+        self.s3_from_user = S3User.objects.create(endpoint=self.endpoint,
+                                                  s3_id='from_user_s3_id',
+                                                  user=self.from_user,
+                                                  type=S3UserTypes.NORMAL)
+        self.s3_to_user = S3User.objects.create(endpoint=self.endpoint,
+                                                s3_id='to_user_s3_id',
+                                                user=self.to_user,
+                                                type=S3UserTypes.NORMAL)
+        self.s3_bucket = S3Bucket.objects.create(name='mouse', owner=self.s3_from_user, endpoint=self.endpoint)
+        self.s3_delivery = S3Delivery.objects.create(bucket=self.s3_bucket,
+                                                     from_user=self.s3_from_user,
+                                                     to_user=self.s3_to_user)
+
+    def test_s3_agent_property(self):
+        s3_delivery_util = S3DeliveryUtil(self.s3_delivery, self.from_user)
+        s3_agent = s3_delivery_util.s3_agent
+        self.assertEqual(s3_agent.type, S3UserTypes.AGENT)
+        self.assertEqual(s3_agent.id, self.s3_agent_user.id)
+
+    def test_s3_current_user_property(self):
+        s3_delivery_util = S3DeliveryUtil(self.s3_delivery, self.from_user)
+        current_s3_user = s3_delivery_util.current_s3_user
+        self.assertEqual(current_s3_user.type, S3UserTypes.NORMAL)
+        self.assertEqual(current_s3_user.id, self.s3_from_user.id)
+
+    def test_destination_bucket_name_property(self):
+        self.s3_bucket.name = 'mouse'
+        self.s3_bucket.save()
+        s3_delivery_util = S3DeliveryUtil(self.s3_delivery, self.from_user)
+        destination_bucket_name = s3_delivery_util.destination_bucket_name
+        self.assertEqual(destination_bucket_name, 'delivery_mouse')
+
+        self.s3_bucket.name = 'ThisBucket'
+        self.s3_bucket.save()
+        s3_delivery_util = S3DeliveryUtil(self.s3_delivery, self.from_user)
+        destination_bucket_name = s3_delivery_util.destination_bucket_name
+        self.assertEqual(destination_bucket_name, 'delivery_ThisBucket')
+
+    @patch('switchboard.s3_util.S3Resource')
+    def test_give_agent_permissions(self, mock_s3_resource):
+        s3_delivery_util = S3DeliveryUtil(self.s3_delivery, self.from_user)
+        s3_delivery_util.give_agent_permissions()
+        mock_s3_resource.return_value.grant_bucket_acl.assert_called_with(
+            'mouse',
+            grant_full_control_user=s3_delivery_util.s3_agent
+        )
+
+    @patch('switchboard.s3_util.S3Resource')
+    def test_accept_project_transfer(self, mock_s3_resource):
+        mock_s3_grant_read = Mock()
+        mock_s3_copy_files = Mock()
+        mock_s3_cleanup_bucket = Mock()
+        mock_s3_resource.side_effect = [
+            mock_s3_grant_read,
+            mock_s3_copy_files,
+            mock_s3_cleanup_bucket
+        ]
+
+        s3_delivery_util = S3DeliveryUtil(self.s3_delivery, self.to_user)
+        s3_delivery_util.accept_project_transfer()
+
+        # First we grant read permissions to to_user while retaining control for agent user
+        mock_s3_grant_read.grant_bucket_acl.assert_called_with(
+            'mouse',
+            grant_full_control_user=self.s3_agent_user,
+            grant_read_user=self.s3_to_user
+        )
+        mock_s3_grant_read.grant_objects_acl.assert_called_with(
+            'mouse',
+            grant_full_control_user=self.s3_agent_user,
+            grant_read_user=self.s3_to_user
+        )
+
+        # As the to user create a bucket and copy the files
+        mock_s3_copy_files.create_bucket.assert_called_with(s3_delivery_util.destination_bucket_name)
+        mock_s3_copy_files.copy_bucket.assert_called_with(s3_delivery_util.source_bucket_name,
+                                                          s3_delivery_util.destination_bucket_name)
+
+        # As the agent Delete the source (this also deletes the files)
+        mock_s3_cleanup_bucket.delete_bucket.assert_called_with(s3_delivery_util.source_bucket_name)
+
+    @patch('switchboard.s3_util.S3Resource')
+    def test_decline_delivery(self, mock_s3_resource):
+        s3_delivery_util = S3DeliveryUtil(self.s3_delivery, self.to_user)
+        s3_delivery_util.decline_delivery()
+        mock_s3_resource.return_value.grant_bucket_acl.assert_called_with(
+            'mouse', grant_full_control_user=self.s3_from_user
+        )
 
 
 class S3ResourceTestCase(TestCase):
@@ -103,6 +203,3 @@ class S3ResourceTestCase(TestCase):
         mock_object_acl_constructor.return_value.put.assert_called_with(
             GrantFullControl='id=user3',
             GrantRead='id=user4')
-
-
-
