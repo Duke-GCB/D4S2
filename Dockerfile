002188816b76bbd48900661e822093f416b7cac6
Dan Leehr <dan.leehr@duke.edu>
2018-05-23 14:41:38 -0400

    Remove section to wait for postgres with psql
    Better implemented as a healthcheck and not useful short-term

--- Dockerfile
+++ Dockerfile
@@ -22,4 +22,4 @@ WORKDIR /app/
 # Collect static files.
 RUN D4S2_SECRET_KEY=DUMMY python manage.py collectstatic --noinput
 
-CMD run.sh
+CMD /app/run.sh
--- run.sh
+++ run.sh
@@ -4,24 +4,11 @@
 
 set -e
 
-# 1. Wait until postgres is ready, via https://docs.docker.com/compose/startup-order/
-
-# Set environment variables for psql to read
-export PGDATABASE=$POSTGRES_DB
-export PGPASSWORD=$POSTGRES_PASSWORD
-export PGUSER=$POSTGRES_USER
-export PGHOST=$POSTGRES_HOST
-
-until psql -c '\l'; do
-  >&2 echo "Postgres is unavailable - sleeping"
-  sleep 1
-done
-
-# 2. Run a django migration
+# 1. Run a django migration
 python manage.py migrate --noinput
 
-# 3. Create/Update DukeDS configuration
+# 2. Create/Update DukeDS configuration
 python manage.py createddsendpoint "Duke Data Service" $D4S2_DDSCLIENT_URL $D4S2_DDSCLIENT_PORTAL_ROOT $D4S2_DDSCLIENT_AGENT_KEY $D4S2_DDSCLIENT_OPENID_PROVIDER_ID
 
-# 4. Launch gunicorn
+# 3. Launch gunicorn
 gunicorn -b 0.0.0.0:8000 d4s2.wsgi:application
