0331b5c824d23143bd44b0b9974909f5683474cb
Dan Leehr <dan.leehr@duke.edu>
2018-05-24 11:22:19 -0400

    Restore postgres readyness check in run.sh

--- run.sh
+++ run.sh
@@ -4,11 +4,17 @@
 
 set -e
 
-# 1. Run a django migration
+# 1. Wait for postgres to be ready
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$POSTGRES_HOST" -U "$POSTGRES_USER" "$POSTGRES_DB" -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+# 2. Run a django migration
 python manage.py migrate --noinput
 
-# 2. Create/Update DukeDS configuration
+# 3. Create/Update DukeDS configuration
 python manage.py createddsendpoint "Duke Data Service" $D4S2_DDSCLIENT_URL $D4S2_DDSCLIENT_PORTAL_ROOT $D4S2_DDSCLIENT_AGENT_KEY $D4S2_DDSCLIENT_OPENID_PROVIDER_ID
 
-# 3. Launch gunicorn
+# 4. Launch gunicorn
 gunicorn -b 0.0.0.0:8000 d4s2.wsgi:application
