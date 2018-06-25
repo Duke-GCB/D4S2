cf0bdcb6cc3b8cfe7143d5164b53a14f69806406
Dan Leehr <dan.leehr@duke.edu>
2018-06-25 15:58:30 -0400

    Prevent duplicate admin registration of DDSUserCredential and DDSEndpoint

--- d4s2_api/admin.py
+++ d4s2_api/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from d4s2_api.models import *
-from gcb_web_auth.models import DDSEndpoint, DDSUserCredential
 from simple_history.admin import SimpleHistoryAdmin
 
 admin.site.register(EmailTemplate)
@@ -20,8 +19,6 @@ class DDSDeliveryAdmin(SimpleHistoryAdmin):
 admin.site.register(DDSDelivery, DDSDeliveryAdmin)
 admin.site.register(DDSDeliveryError)
 admin.site.register(DDSDeliveryShareUser)
-admin.site.register(DDSEndpoint)
-admin.site.register(DDSUserCredential)
 admin.site.register(EmailTemplateSet)
 admin.site.register(UserEmailTemplateSet)
 admin.site.register(S3Endpoint)
