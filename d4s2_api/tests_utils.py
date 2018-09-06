6e5fc41fbe2b495e973d2989ca2f59d20f1f29dc
John Bradley <johnbradley2008@gmail.com>
2018-09-06 09:03:11 -0400

    tests for MessageFactory.make_processed_message

--- d4s2_api/tests_utils.py
+++ d4s2_api/tests_utils.py
@@ -65,7 +65,17 @@ class MessageFactoryTestCase(TestCase):
         mock_message.assert_called_with('bob@bob.com', 'joe@joe.com', 'actionsubject', 'actionbody', {})
 
     @patch('d4s2_api.utils.Message')
-    def test_make_processed_message(self, mock_message):
+    def test_make_processed_message_to_sender(self, mock_message):
         factory = MessageFactory(self.delivery_details)
-        factory.make_processed_message('accept', warning_message='warning details')
+        factory.make_processed_message('accepted', warning_message='warning details',
+                                       direction=MessageDirection.ToSender)
+        mock_message.assert_called_with('joe@joe.com', 'bob@bob.com', 'actionsubject', 'actionbody', {})
+        self.delivery_details.get_email_context.assert_called_with(None, 'accepted', '', 'warning details')
+
+    @patch('d4s2_api.utils.Message')
+    def test_make_processed_message_to_recipient(self, mock_message):
+        factory = MessageFactory(self.delivery_details)
+        factory.make_processed_message('accepted_recipient', warning_message='warning details',
+                                       direction=MessageDirection.ToRecipient)
         mock_message.assert_called_with('bob@bob.com', 'joe@joe.com', 'actionsubject', 'actionbody', {})
+        self.delivery_details.get_email_context.assert_called_with(None, 'accepted_recipient', '', 'warning details')
