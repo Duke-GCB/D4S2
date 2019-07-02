63f59276d69d6a1e3f8a3cb5e222651cac62e696
Dan Leehr <dan.leehr@duke.edu>
2019-07-02 11:04:31 -0400

    Update run.sh to use gevent workers for streaming downloads
    Per https://docs.gunicorn.org/en/stable/deploy.html?highlight=streaming
    
    > If you want to be able to handle streaming request/responses or other fancy features like Comet, Long polling, or Web sockets, you need to turn off the proxy buffering. When you do this you must run with one of the async worker classes.
    
    > To turn off buffering, you only need to add proxy_buffering off; to your location block:

--- run.sh
+++ run.sh
@@ -22,4 +22,6 @@ python manage.py migrate --noinput
 python manage.py createddsendpoint "Duke Data Service" $D4S2_DDSCLIENT_URL $D4S2_DDSCLIENT_PORTAL_ROOT $D4S2_DDSCLIENT_AGENT_KEY $D4S2_DDSCLIENT_OPENID_PROVIDER_SERVICE_ID $D4S2_DDSCLIENT_OPENID_PROVIDER_ID
 
 # 4. Launch gunicorn
-gunicorn -b 0.0.0.0:8000 d4s2.wsgi:application
+# gevent workers are async and needed to service streaming downloads
+# with the default (sync) workers, streaming download responses time out.
+gunicorn -b 0.0.0.0:8000 --worker-class gevent d4s2.wsgi:application
