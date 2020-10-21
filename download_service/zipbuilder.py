d4292d7f5bf96c09b281cc9ec27abba5ca194251
John Bradley <johnbradley2008@gmail.com>
2020-10-21 14:47:29 -0400

    start downloads faster
    Enables faster downloads by batching requests for files from DDS and sending
    file data as soon as it is ready.  Reads DDS files a batch at a time using
    the get_project_files_generator generator. Uses yield/flush technique from
    zipstream-new to start sending files when ready. Previously a zip download
    would not start until details about all files had been read.

--- download_service/zipbuilder.py
+++ download_service/zipbuilder.py
@@ -1,7 +1,17 @@
 from zipstream import ZipFile
 from ddsc.sdk.client import PathToFiles
 from ddsc.core.ddsapi import DataServiceError
+from ddsc.core.download import SWIFT_EXPIRED_STATUS_CODE, S3_EXPIRED_STATUS_CODE
 import requests
+
+
+def is_expired_dds_response(response):
+    """
+    Check status_code of the response for the two values denoting expired URLs
+    :param response: requests.Response
+    :return: bool: True when the response is expired status
+    """
+    return response.status_code == SWIFT_EXPIRED_STATUS_CODE or response.status_code == S3_EXPIRED_STATUS_CODE
 
 
 class ZipBuilderException(BaseException):
@@ -29,6 +39,7 @@ class DDSZipBuilder(object):
         """
         self.project_id = project_id
         self.client = client
+        self.page_size = client.dds_connection.config.page_size
 
     @staticmethod
     def _handle_dataservice_error(e, message):
@@ -67,53 +78,47 @@ class DDSZipBuilder(object):
         if filename != expected_filename:
             raise NotFoundException('Project {} not found'.format(self.project_id))
 
-    def get_dds_paths(self):
+    def get_project_file_generator(self):
         """
-        Builds a mapping of file paths (strings) in the dds project to ddsc.sdk.client.File objects
-        :return: OrderedDict where keys are relative paths in the project and values are ddsc.sdk.client.File objects
+        Returns a generator that returns DukeDS files for the current project including a download url
+        :return: generator yielding ddsc.core.remotestore.ProjectFile, requests.Response.headers tuples
         """
         try:
             project = self.client.get_project_by_id(self.project_id)
-            ptf = PathToFiles()
-            ptf.add_paths_for_children_of_node(project)
-            return ptf.paths  # OrderedDict of path -> File
+            return project.get_project_files_generator(page_size=self.page_size)
         except DataServiceError as e:
             DDSZipBuilder._handle_dataservice_error(e, 'Project {} not found'.format(self.project_id))
 
-    def get_url(self, dds_file):
+    def get_url(self, file_id):
         """
-        Uses the client to get a download URL for a ddsc.sdk.client.File object. This URL will be signed with an
+        Generate a file download URL for a DDS file id. This URL will be signed with an
         expiration date, so this method should only be called right before the URL will be fetched.
-
-        :param dds_file: A ddsc.sdk.client.File object for which to get a URL
+        :param file_id: str: DDS file id
         :return: The URL string to GET.
         """
-        # This is a time-sensitive call, so we should only do it right before fetch
         try:
-            file_download = self.client.dds_connection.get_file_download(dds_file.id)
+            file_download = self.client.dds_connection.get_file_download(file_id)
             if file_download.http_verb == 'GET':
                 return '{}{}'.format(file_download.host, file_download.url)
             else:
                 raise NotSupportedException('This file requires an unsupported download method: {}'.format(file_download.http_verb))
         except DataServiceError as e:
-            DDSZipBuilder._handle_dataservice_error(e, 'File with id {} not found'.format(dds_file.id))
+            DDSZipBuilder._handle_dataservice_error(e, 'File with id {} not found'.format(file_id))
 
-    def fetch(self, dds_file):
+    def fetch(self, project_file):
         """
         Generator to provide the contents of the DDS file using requests.get with a streaming response.
-
-        Note: Since self.get_url() returns a URL that will expire, it's critical that the yield and the get_url()
-        call appear in the same function. The 'yield' in this function actually causes the get_url() call to be deferred
-        until the first time the generator is consumed. If the yield were in a nested function, get_url() would be
-        called immediately and the URL could easily expire before it's fetched.
-
-        :param dds_file: A ddsc.sdk.client.File object to fetch
+        :param project_file: ddsc.core.remotestore.ProjectFile containing file info and a potentially expired url
         :return: generator, bytes of the DDS file fetched from its URL.
         """
         # Due to the specifics of how python generators work, we have to make sure the yield appears in the same
-        # function as the call to self.get_url().
-        url = self.get_url(dds_file)
+        # function as the call to self.get_url()
+        url = project_file.file_url['host'] + project_file.file_url['url']
         response = requests.get(url, stream=True)
+        if is_expired_dds_response(response):
+            url = self.get_url(project_file.id)
+            response = requests.get(url, stream=True)
+        response.raise_for_status()
         for chunk in response.raw.stream():
             yield chunk
 
@@ -126,9 +131,8 @@ class DDSZipBuilder(object):
         # Set allowZip64 explicitly - it normally looks at file size but since we're building on the fly
         # we don't know that ahead of time
         zipfile = ZipFile(allowZip64=True)
-        paths = self.get_dds_paths()
-        for (filename, dds_file) in paths.items():
+        for project_file, _ in self.get_project_file_generator():
             # Must provide buffer_size so that write_iter will know to use zip64
-            file_size = dds_file.current_version['upload']['size']
-            zipfile.write_iter(filename, self.fetch(dds_file), buffer_size=file_size)
-        return zipfile
+            zipfile.write_iter(project_file.path, self.fetch(project_file), buffer_size=project_file.size)
+            yield from zipfile.flush()
+        yield from zipfile
